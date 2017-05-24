a523a2e9c537eaa067de0085463af95abab0bad6
daturach <daturach@users.noreply.github.com>
2017-05-24 15:00:29 +0200

    Update config.h

--- portable_thermometer/config.h
+++ portable_thermometer/config.h
@@ -1,1 +1,14 @@
+const char* ssid[] = 
+{
+    [0] = "your_ssid_0",
+    [1] = "your_ssid_1",
+    [2] = "your_ssid_2"
+};
+const char* password[] = 
+{
+    [0] = "your_password_0",
+    [1] = "your_password_1",
+    [2] = "your_password_2"
+};    
 
+const char* privateKey = "your_key_4_emoncms.org";
